9c0363115888800325da1a826d4c403fb6a9da42
canalmedia <info@canalmedia.eu>
2020-06-01 16:34:38 +0200

    Add files via upload

---
+++ catalog/language/sk-sk/extension/shipping/zasilkovna.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_title'] = 'Zásielkovňa';
+$_['choose_branch'] = 'Vyberte pobočku';
+$_['no_branch_selected'] = 'Nie je vybraná žiadna pobočka.';
+
+$_['shipping_cz'] = 'Zásielkovňa - doručenie do ČR';
+$_['shipping_sk'] = 'Zásielkovňa - doručenie na Slovensko';
+$_['shipping_pl'] = 'Zásielkovňa - doručenie do Poľska';
+$_['shipping_hu'] = 'Zásielkovňa - doručenie do Maďarska';
+$_['shipping_ro'] = 'Zásielkovňa - doručenie do Rumunska';
+$_['shipping_other'] = 'Zásielkovňa - doručenie do ostatných zemí';
+$_['shipping_any'] = 'Zásielkovňa';
