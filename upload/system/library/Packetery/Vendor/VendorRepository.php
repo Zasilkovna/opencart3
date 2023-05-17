5bc0043f701a0b6984c03bf782343a2ae8ac5853
Mrakor <20678997+Mrakor@users.noreply.github.com>
2023-05-17 07:22:58 +0200

    PES-1393 Add onlyEnabled parameter to VendorRepository::getVendorsByCountry (#74)
    * PES-1393 Add onlyEnabled parameter to VendorRepository::getVendorsByCountry
    
    * PES-1393 Add columns country, group

--- upload/system/library/Packetery/Vendor/VendorRepository.php
+++ upload/system/library/Packetery/Vendor/VendorRepository.php
@@ -18,19 +18,35 @@ class VendorRepository {
 
 	/**
 	 * @param string $country
+	 * @param bool $onlyEnabled
 	 *
 	 * @return null|array
 	 */
-	public function getVendorsByCountry($country) {
+	public function getVendorsByCountry($country, $onlyEnabled = false) {
 		$countryCode = $this->db->escape($country);
 
+		$additionalWhere = '';
+		if ($onlyEnabled) {
+			$additionalWhere = ' AND `zv`.`is_enabled` = 1 ';
+		}
+
 		$query = sprintf(
-			"SELECT `zv`.`carrier_id`, `zv`.`id` AS `vendor_id`, `zc`.`name`, `zv`.`carrier_name_cart`  
+			"SELECT `zv`.`carrier_id`,
+				`zv`.`id` AS `vendor_id`,
+				`zc`.`name`,
+				`zv`.`carrier_name_cart`,
+				`zv`.`group`,
+				COALESCE(`zv`.`country`, `zc`.`country`) AS `country`
 			FROM `%s` `zv` 
 			LEFT JOIN `%s` `zc` ON `zv`.`carrier_id` = `zc`.`id`
-			WHERE `zv`.`country` = '%s' OR `zc`.`country` = '%s'
+			WHERE (`zv`.`country` = '%s' OR `zc`.`country` = '%s') %s
 			UNION
-			SELECT `zc`.`id`, `zv`.`id` AS `vendor_id`, `zc`.`name`, `zv`.`carrier_name_cart`
+			SELECT `zc`.`id`,
+				`zv`.`id` AS `vendor_id`,
+				`zc`.`name`,
+				`zv`.`carrier_name_cart`,
+				null AS `group`,
+				`zc`.`country`
 			FROM `%s` `zc`
 			LEFT JOIN `%s` `zv` ON `zv`.`carrier_id` = `zc`.`id`
 			WHERE `zc`.`country` = '%s' AND `zc`.`deleted` = 0
@@ -39,6 +55,7 @@ class VendorRepository {
 			DB_PREFIX . 'zasilkovna_carrier',
 			$countryCode,
 			$countryCode,
+			$additionalWhere,
 			DB_PREFIX . 'zasilkovna_carrier',
 			DB_PREFIX . 'zasilkovna_vendor',
 			$countryCode
