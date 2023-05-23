9f2fc904769aadbe2902c905117cf5b7628c2b37
Mrakor <20678997+Mrakor@users.noreply.github.com>
2023-05-23 20:19:26 +0200

    PES-1393 Fix has_pickup_points in getVendorByCountry (#78)
    * PES-1393 Fix has_pickup_points in getVendorByCountry
    
    * PES-1393 Cr fix - only rows from vendor table

--- upload/system/library/Packetery/Vendor/VendorRepository.php
+++ upload/system/library/Packetery/Vendor/VendorRepository.php
@@ -37,30 +37,16 @@ class VendorRepository {
 				`zv`.`carrier_name_cart`,
 				`zv`.`group`,
 				COALESCE(`zv`.`country`, `zc`.`country`) AS `country`,
-				IF(ISNULL(`zv`.`carrier_id`), 1, 0) AS 'has_pickup_points'
+				IF(ISNULL(`zv`.`carrier_id`), 1, `zc`.`is_pickup_points`) AS 'has_pickup_points'
 			FROM `%s` `zv` 
 			LEFT JOIN `%s` `zc` ON `zv`.`carrier_id` = `zc`.`id`
 			WHERE (`zv`.`country` = '%s' OR `zc`.`country` = '%s') %s
-			UNION
-			SELECT `zc`.`id`,
-				`zv`.`id` AS `vendor_id`,
-				`zc`.`name`,
-				`zv`.`carrier_name_cart`,
-				null AS `group`,
-				`zc`.`country`,
-				`zc`.`is_pickup_points` AS `has_pickup_points`
-			FROM `%s` `zc`
-			LEFT JOIN `%s` `zv` ON `zv`.`carrier_id` = `zc`.`id`
-			WHERE `zc`.`country` = '%s' AND `zc`.`deleted` = 0
 			",
 			DB_PREFIX . 'zasilkovna_vendor',
 			DB_PREFIX . 'zasilkovna_carrier',
 			$countryCode,
 			$countryCode,
-			$additionalWhere,
-			DB_PREFIX . 'zasilkovna_carrier',
-			DB_PREFIX . 'zasilkovna_vendor',
-			$countryCode
+			$additionalWhere
 		);
 
 		$queryResult = $this->db->query($query);
