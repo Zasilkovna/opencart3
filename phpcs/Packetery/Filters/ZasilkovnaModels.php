98f63f7fc0609496e45dbf33954be05b7ce4feff
Mrakor <20678997+Mrakor@users.noreply.github.com>
2023-06-20 09:24:21 +0200

    PES-1379 CS fix of filter

--- phpcs/Packetery/Filters/ZasilkovnaModels.php
+++ phpcs/Packetery/Filters/ZasilkovnaModels.php
@@ -7,19 +7,22 @@ namespace PHP_CodeSniffer\Filters;
 use PHP_CodeSniffer\Util;
 
 class ZasilkovnaModels extends Filter {
-	const OC3_MODEL_EXTENSION_SHIPPING_DIR = 'upload/admin/model/extension/shipping';
+    const OC3_MODEL_EXTENSION_SHIPPING_DIR = 'upload/admin/model/extension/shipping';
 
-	/**
-	 * Doesn't accept files that are not zasilkovna*.php files and are located in OC3 model directory.
-	 * @return bool
-	 */
-	public function accept() {
-		if (parent::accept() === false) {
-			return false;
-		}
+    /**
+     * Doesn't accept files that are not zasilkovna*.php files and are located in OC3 model directory.
+     * @return boolean
+     */
+    public function accept() {
+        if (parent::accept() === false) {
+            return false;
+        }
 
-		$filePath = Util\Common::realpath($this->current());
+        $filePath = Util\Common::realpath($this->current());
 
-		return !((strpos($filePath, self::OC3_MODEL_EXTENSION_SHIPPING_DIR) !== false) && strpos($filePath, 'zasilkovna') === false);
-	}
+        return !(
+            (strpos($filePath, self::OC3_MODEL_EXTENSION_SHIPPING_DIR) !== false)
+            && strpos($filePath, 'zasilkovna') === false
+        );
+    }
 }
