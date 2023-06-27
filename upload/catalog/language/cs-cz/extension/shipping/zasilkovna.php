379320fb0b518202cba268bbd9b88ec995daf8c9
Mrakor <20678997+Mrakor@users.noreply.github.com>
2023-06-27 11:31:20 +0200

    PES-1454 Get frontend translations in sync

--- upload/catalog/language/cs-cz/extension/shipping/zasilkovna.php
+++ upload/catalog/language/cs-cz/extension/shipping/zasilkovna.php
@@ -4,13 +4,7 @@ $_['text_title'] = 'Zásilkovna';
 $_['choose_branch'] = 'Vyberte pobočku';
 $_['no_branch_selected'] = 'Není vybrána žádná pobočka.';
 
-$_['shipping_cz'] = 'Zásilkovna - doručení do ČR';
-$_['shipping_sk'] = 'Zásilkovna - doručení na Slovensko';
-$_['shipping_pl'] = 'Zásilkovna - doručení do Polska';
-$_['shipping_hu'] = 'Zásilkovna - doručení do Maďarska';
-$_['shipping_ro'] = 'Zásilkovna - doručení do Rumunska';
-$_['shipping_other'] = 'Zásilkovna - doručení do ostatních zemí';
-$_['shipping_any'] = 'Zásilkovna';
+$_['shipping'] = 'Zásilkovna';
 
 // Carriers update
 $_['carriers_updated'] = 'Seznam dopravců byl aktualizován.';
